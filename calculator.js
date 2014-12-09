aa9e060f5cdafef2cdf8e4c5415945cb4c98173d
William Hurst <wbhurst3@gmail.com>
2014-12-09 18:23:23 -0500

    OOP refactor // Add mult and division
    Refactored code to be Object Oriented with basic prototype. Added
    multiplication and division functions. Results now show up singly in
    results box, emulating handheld calculators.

--- calculator.js
+++ calculator.js
@@ -1,69 +1,132 @@
-var num_list = [];
-
-// Takes in a number and displays it in the results box
-function display(num) {
-	var display_num = num;
-	if (! document.getElementById("results").value) {
-		document.getElementById("results").value = display_num;
-	} else {
-		document.getElementById("results").value += display_num;
+var CalcPrototype = {
+	constructor: function() {
+		this.num_list = []; // For holding the list of numbers to be operated on
+		this.last_op = ""; // For function hash to call current operation
+		this.current_num = ""; // For holding the current single or multi-digit number
+	},
+	// Takes in a number and displays it in the results box
+	display: function(num){
+		if (! this.current_num) {
+			this.current_num = num;
+			if (num !== 0){
+				document.getElementById("results").value = num;
+			}
+		} else {
+			document.getElementById("results").value += num;
+			this.current_num += num;
+		}
+	},
+	// Convenience fn for finding sum
+	sum: function(list){
+		var total = parseFloat(list[0]) + parseFloat(this.num_list.pop());
+		this.num_list[0] = total
+	},
+	// Convenience fn for finding difference
+	difference: function(list){
+		var total = parseFloat(list[0]) - parseFloat(this.num_list.pop());
+		this.num_list[0] = total
+	},
+	prod: function(list){
+		var total = parseFloat(list[0]) * parseFloat(this.num_list.pop());
+		this.num_list[0] = total
+	},
+	quotient: function(list){
+		var total = parseFloat(list[0]) / parseFloat(this.num_list.pop());
+		this.num_list[0] = total
+	},
+	// Adds the number in the results box to the array of items to be operated on
+	add: function() {
+		if (this.current_num){
+			// Add current number to the list
+			this.num_list.push(parseFloat(this.current_num));
+			this.current_num = "";
+			calculator.runLastOp(this.last_op);
+			// Declare current operation done
+			this.last_op = "add";
+			// Append operation to results box
+			document.getElementById("results").value = this.num_list.toString();
+		}
+	},
+	// Subtracts the number in results box from sum so far
+	subtract: function() {
+		if (this.current_num){
+			// Add current number to the list
+			this.num_list.push(parseFloat(this.current_num));
+			this.current_num = "";
+			// Run current operation
+			calculator.runLastOp(this.last_op);
+			// Declare current operation done
+			this.last_op = "subtract";
+			// Append operation to results box
+			document.getElementById("results").value = this.num_list.toString();
+		}
+	},
+	// Multiplies the number in results box from sum so far
+	multiply: function() {
+		if (this.current_num){
+			// Add current number to the list
+			this.num_list.push(parseFloat(this.current_num));
+			this.current_num = "";
+			// Run current operation
+			calculator.runLastOp(this.last_op);
+			// Declare current operation done
+			this.last_op = "multiply";
+			// Append operation to results box
+			document.getElementById("results").value = this.num_list.toString();
+		}
+	},
+	// Divides the number in results box from sum so far
+	divide: function() {
+		if (this.current_num){
+			// Add current number to the list
+			this.num_list.push(parseFloat(this.current_num));
+			this.current_num = "";
+			// Run current operation
+			calculator.runLastOp(this.last_op);
+			// Declare current operation done
+			this.last_op = "divide";
+			// Append operation to results box
+			document.getElementById("results").value = this.num_list.toString();
+		}
+	},
+	// Does the specified operations to the numbers
+	solve: function() {
+		if (this.current_num){
+			// Add current number to the list
+			this.num_list.push(parseFloat(this.current_num));
+			this.current_num = "";
+			calculator.runLastOp(this.last_op);
+			// Clear last operation var
+			// this.last_op = "";
+			// Append operation to results box
+			document.getElementById("results").value = this.num_list.toString();
+		}
+	},
+	// Deletes last number entered
+	backsp: function(){
+		this.current_num = this.current_num.slice(0, this.current_num.length-1);
+		document.getElementById("results").value = document.getElementById("results").value.slice(0, document.getElementById("results").value.length-1)
+	},
+	// Clears the results box and num_list
+	wipe: function() {
+		document.getElementById("results").value = "";
+		// Clear number list
+		this.num_list = [];
+		// Clear last operation pressed
+		this.last_op = "";
+		// Clear any number currently queued
+		this.current_num = "";
+	},
+	runLastOp: function(operation){
+		// Switch for determining which operation to run. Takes last_op as string
+		switch(operation){
+			case "add" : calculator.sum(this.num_list); break;
+			case "subtract" : calculator.difference(this.num_list); break;
+			case "multiply" : calculator.prod(this.num_list); break;
+			case "divide" : calculator.quotient(this.num_list); break;
+		}
 	}
-	
 }
 
-// Clears the results box and num_list
-function wipe() {
-	document.getElementById("results").value = "";
-	// Clear number list
-	num_list = [];
-	// Clear results paragraph
-	document.getElementById("numbersList").innerHTML = "";
-}
-
-// Adds the number in the results box to the array of items to be operated on
-function add() {
-	// Assign value of results to variable
-	var next_num = document.getElementById("results").value;
-	// Add to the list
-	num_list.push(next_num);
-	// Clear the results box
-	document.getElementById("results").value = "";
-	// Convert array to string and assign to variable
-	var raw_nums = num_list.toString();
-	// Replace commas with plus sign, using Regex
-	var clean_nums = raw_nums.replace(/,/g, " + ");
-	// Assign value to paragraph
-	document.getElementById("numbersList").innerHTML = clean_nums;
-}
-
-// Subtracts the number in results box from sum so far
-function subtract() {
-	
-}
-
-// Finds the difference of two numbers for subtract function
-function difference(a, b) {
-	return a - b;
-}
-
-// Method for calculating the sum of num_list
-function sum(list) {
-	var total = 0;
-	// Loop through list and produce total
-	for (var i = 0; i < list.length; i++) {
-		total += parseInt(list[i], 10);
-	};
-	return total;
-}
-
-// Does the specified operations to the numbers
-function solve() {
-	// Assign value of results to variable
-	var next_num = document.getElementById("results").value;
-	// Add to the list
-	num_list.push(next_num);
-	// Clear the results box
-	document.getElementById("results").value = "";
-	// Sum and assign value to paragraph
-	document.getElementById("numbersList").innerHTML = "Solution: " + sum(num_list);
-}
+var calculator = Object.create(CalcPrototype);
+calculator.constructor();
